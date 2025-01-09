1b304a79be12c6399ab301907b38971bf0791ad2
Phillemon Haikali 2 <maxnda@gmail.com>
2025-01-10 01:29:01 +0200

    still

--- week1/basics.py
+++ week1/basics.py
@@ -1,6 +1,6 @@
 import cv2
-image = cv2.imread(r"C:\Users\Max\Documents\Projects\opencv-mediapipe\week 1\image.jpeg")
-gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)  # Also fixed the comma and constant here
+image-cv2.imread(r"C:\Users\Max\Documents\Projects\opencv-mediapipe\week 1\image.jpeg")
+gray_image=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 cv2.imshow('Grayscale image', gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
