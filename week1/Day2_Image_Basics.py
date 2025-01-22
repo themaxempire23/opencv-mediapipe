c9f12bb2c4a1de7a3d2afa4cec2f16d47d575e7a
Max <maxnda@gmail.com>
2025-01-22 23:33:48 +0200

    showing the image in 3 different shades

--- week1/Day2_Image_Basics.py
+++ week1/Day2_Image_Basics.py
@@ -1,6 +1,28 @@
 import cv2image
 image=cv2.imread(r"C:\Users\Max\Documents\Projects\opencv-mediapipe\week1\image.jpeg")
 gray_image=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+
+cv2.destroyAllWindows()
+
+HSV_image=cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+
+LAB_image=cv2.cvtColor(image,cv2.COLOR_RGB2Lab)
+
+#Showing the image to the user within 4 tabs
+
+# 1st showing the grayscale image
 cv2.imshow('Grayscale image',gray_image)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+
+# 2nd showing the HSV image
+cv2.imshow('HSV image',HSV_image)
+cv2.waitKey(0)
+
+
+# 3rd showing the Lab image
+cv2.imshow('LAB image',LAB_image)
+cv2.waitKey(0)
+
+
+
