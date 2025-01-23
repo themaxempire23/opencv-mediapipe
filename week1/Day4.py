c6e6fa5b1681167ace4859d8a118c770885c5a44
Max <maxnda@gmail.com>
2025-01-23 23:52:35 +0200

    drew rectangle

--- week1/Day4.py
+++ week1/Day4.py
@@ -6,6 +6,7 @@ def draw_shapes_and_text():
     img = np.zeros((500, 800, 3), dtype=np.uint8) # declaring img variable followed an entry of zeros
 
     cv2.line(img, (50, 50),( 500, 100),(0, 255, 0), 5)
+    cv2.rectangle(img, (50, 50),( 500, 100),(0, 255, 0), 5)
 
     cv2.imshow("Edit",img)
     cv2.waitKey(0)
