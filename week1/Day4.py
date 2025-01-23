64f5c66fc022c162621b43937570ab8b85a107f1
Max <maxnda@gmail.com>
2025-01-23 23:45:10 +0200

    not working

---
+++ week1/Day4.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+def draw_shapes_and_text():
+    # Creating blank image
+    img - np.zeros((500, 800, 3), dtype-np.uint8) # declaring img variable followed an entry of zeros
+
+    cv2.line(img,(50,50),(50,100),(0,255,0),5)
+
+    cv2.imshow("Edit",img)
+    cv2.waitKey(0)
+
+    draw_shapes_and_text()
