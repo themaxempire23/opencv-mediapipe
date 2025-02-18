2df6793c53ec0c42b409a30c3c4c710c3fc19fb4
max <Phillemon.Haikali@transnamib.com.na>
2025-02-18 18:34:50 +0200

    Corner Harris done

--- week2/Day10.py
+++ week2/Day10.py
@@ -1,3 +1,5 @@
+# Corner Harris
+
 import cv2
 import numpy as np
 
@@ -10,7 +12,13 @@ if img is None:
 
 gray =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Conerterting to grayscale
 
-gray
+gray=np.float32(gray)
+
+dst=cv2.cornerHarris(gray,2,3,0..04)
+
+dst=cv2.dilate(dst,None)
+img[dst>0.01*dst.max()]=[0,255,0]
+
 # Show the results
 cv2.imshow("Harris Corners", img)
 cv2.waitKey(0)
